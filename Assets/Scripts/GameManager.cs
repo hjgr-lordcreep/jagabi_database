84900d7a7b18944799e0b421e2a17bc647111862
DESKTOP-3SIE30Q\user <kjy2000212@gmail.com>
2024-11-20 16:40:02 +0900

    씬 2 회원가입 완료

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.gameObject);
+                if(hit.transform.CompareTag("Items"))
+                    hit.transform.gameObject.SetActive(false);
+            }
+        }
+
+        //µð¹ö±ë¿ë
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Jagabee3");
+
+        }
+    }
+}
