01fda54c302f6d99af20489a08a3354ff569102e
lordcreep <hjgr.lordcreep@gmail.com>
2024-11-21 14:45:15 +0900

    no message

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -4,16 +4,16 @@ using UnityEngine.UI;
 
 public class InventoryItem : MonoBehaviour
 {
-    //public void Init(Item.ItemInfo _itemInfo)
-    //{
-    //    Image img = GetComponent<Image>();
-    //    img.color = _itemInfo.RareToColor();
-    //}
-
-    Items items = new Items();
-    public void Init()
+    public void Init(Item.ItemInfo _itemInfo)
     {
         Image img = GetComponent<Image>();
-        img.color = items.itemColor;
+        img.color = _itemInfo.RareToColor();
     }
+
+    //Items items = new Items();
+    //public void Init()
+    //{
+    //    Image img = GetComponent<Image>();
+    //    img.color = items.itemColor;
+    //}
 }
